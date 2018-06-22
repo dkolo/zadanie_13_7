24b6ec27d80cc01edc0463f1a0c0aa6c9e5c784c
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-22 19:58:29 +0200

    Add readFile functions

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function(request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-        response.end();
+
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            response.write(data);
+            return response.end();
+        });
     } else {
-        response.statusCode = 404;
-        response.write('<h1>404: Zła ścieżka!</h1>');
-        response.end();
+        fs.readFile('404errorpage.jpg', function(err, data) {
+            response.write(data);
+            return response.end();
+        });
     }
 });
-
 server.listen(8080);
